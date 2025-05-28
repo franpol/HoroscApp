4931f4e0416bd531cd348aa631dead3896f7e7ed
Franco <fpollano@gmail.com>
2025-05-28 00:06:10 -0300

    se agregaron los fragments y el binding

---
+++ app/src/main/java/com/aristidevs/horoscapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,21 @@
+package com.aristidevs.horoscapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aristidevs.horoscapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment() {
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding  = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+}
