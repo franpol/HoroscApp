c5a38617f04aebb7abdc77415bdf8aa99c78862c
Franco <fpollano@gmail.com>
2025-06-07 21:27:02 -0300

    se dejo andando el bottomnav

--- app/src/main/java/com/aristidevs/horoscapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/aristidevs/horoscapp/ui/home/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.aristidevs.horoscapp.R
 import com.aristidevs.horoscapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +27,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initUI()
+    }
+
+    private fun initUI() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
