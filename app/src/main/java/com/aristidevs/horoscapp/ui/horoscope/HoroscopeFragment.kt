ab7591b7a4712b2b5e82b9b512a6873ec493b02a
Franco <fpollano@gmail.com>
2025-06-16 23:31:45 -0300

    se comenzo a implementar el stateflow del horoscopo

--- app/src/main/java/com/aristidevs/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/aristidevs/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,13 +1,18 @@
 package com.aristidevs.horoscapp.ui.horoscope
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.aristidevs.horoscapp.databinding.FragmentHoroscopeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
@@ -15,6 +20,26 @@ class HoroscopeFragment : Fragment() {
 
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+     initUi()
+    }
+
+    private fun initUi() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle (Lifecycle.State.STARTED){
+                horoscopeViewModel.horoscope.collect {
+                    Log.i("franpol", it.toString())
+                }
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
--- app/src/main/java/com/aristidevs/horoscapp/ui/horoscope/HoroscopeViewModel.kt
+++ app/src/main/java/com/aristidevs/horoscapp/ui/horoscope/HoroscopeViewModel.kt
@@ -1,10 +1,22 @@
 package com.aristidevs.horoscapp.ui.horoscope
 
 import androidx.lifecycle.ViewModel
+import com.aristidevs.horoscapp.domain.model.HoroscopeInfo
+import com.aristidevs.horoscapp.domain.model.HoroscopeInfo.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class HoroscopeViewModel @Inject constructor() : ViewModel(){
+class HoroscopeViewModel @Inject constructor() : ViewModel() {
+
+    private val _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
+    val horoscope: StateFlow<List<HoroscopeInfo>> = _horoscope
+
+    init {
+        _horoscope.value = listOf(Aries, Taurus, Gemini)
+
+    }
 
 }
