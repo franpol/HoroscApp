5576553bf055c8a52edc7eb3095cf094883b1e6f
Franco <fpollano@gmail.com>
2025-06-24 23:11:40 -0300

    se implemento navegacion con argumento a HoroscopeDetail

--- app/src/main/java/com/aristidevs/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/aristidevs/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -11,9 +11,13 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aristidevs.horoscapp.databinding.FragmentHoroscopeBinding
+import com.aristidevs.horoscapp.domain.model.HoroscopeInfo
+import com.aristidevs.horoscapp.domain.model.HoroscopeInfo.*
+import com.aristidevs.horoscapp.domain.model.HoroscopeModel
 import com.aristidevs.horoscapp.ui.horoscope.adapter.HoroscopeAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -38,8 +42,27 @@ class HoroscopeFragment : Fragment() {
     }
 
     private fun intiList() {
-        horoscopeAdapter = HoroscopeAdapter(onItemSelected = { Toast.makeText(context,getString(it.name),
-            Toast.LENGTH_LONG).show()})
+        horoscopeAdapter = HoroscopeAdapter(onItemSelected = {
+            //Toast.makeText(context,getString(it.name), Toast.LENGTH_LONG).show()
+            val type = when (it) {
+                Aquarius -> HoroscopeModel.Aquarius
+                Aries -> HoroscopeModel.Aries
+                Cancer -> HoroscopeModel.Cancer
+                Capricorn -> HoroscopeModel.Capricorn
+                Gemini -> HoroscopeModel.Gemini
+                Leo -> HoroscopeModel.Leo
+                Libra -> HoroscopeModel.Libra
+                Pisces -> HoroscopeModel.Pisces
+                Sagittarius -> HoroscopeModel.Sagittarius
+                Scorpio -> HoroscopeModel.Scorpio
+                Taurus -> HoroscopeModel.Taurus
+                Virgo -> HoroscopeModel.Virgo
+            }
+            findNavController().navigate(
+                HoroscopeFragmentDirections.actionHoroscopeFragmentToHoroscopeDetailActivity()
+            )
+        })
+
         binding.rvHoroscope.apply {
             layoutManager = GridLayoutManager(context, 2)
             adapter = horoscopeAdapter
